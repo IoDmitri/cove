f561fa57bddb0dc1ab2d789750b4992ce6c187f9
IoDmitri <Dmitri.Io@utexas.edu>
2017-08-18 02:15:12 -0700

    Added mnt_model_trainer and setup debug sequences

---
+++ nmt_model_trainer.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string("mode", "debug",
+                           """
+                           The mode in which to run. Available modes are "train", and "debug" [train]
+                           """)
+tf.app.flags.DEFINE_integer("enc_h", 300,
+                            """
+                            The hidden size of the encoder [300]
+                            """)
+tf.app.flags.DEFINE_integer("dec_h", 300,
+                            """
+                            the hidden size of the decoder [300]
+                            """)
+
+def main(argv=None):
+    if FLAGS.mode == "debug":
+        parallel_dict = {
+            "a": "z",
+            "b": "y",
+            "c": "x",
+            "d": "w"
+        }
+
+        choices = list(parallel_dict.keys())
+
+        initial_sequences = [[choices[np.random.choice(len(choices), 1)[0]] for _ in range(10)] for _ in range(10)]
+        initial_targets = [[parallel_dict[letter] for letter in seq] for seq in initial_sequences]
+        
+
+
